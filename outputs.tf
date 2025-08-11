193299497098c2a13d092fe119a38a7fd0aad6f1
Vincent Misson <git@misson.cloud>
2025-08-11 11:37:57 +0200

    Fix resource group name retrieval in outputs.tf to ensure correct value extraction from module

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
 output "resource_group_name" {
   description = "The name of the resource group where the resources are deployed."
-  value       = module.netperf.resource_group_name
+  value       = values(module.netperf)[0].resource_group_name
 }
